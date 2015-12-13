ab670346f2685d2739c0cc315516f37de1d15c1e
Petr Kulhanek <kulhanek@chemi.muni.cz>
2015-12-13 15:44:00 +0100

    build update

--- src/lib/cnetcdf/config-gen.h
+++ src/lib/cnetcdf/config-gen.h
@@ -2,8 +2,8 @@
 #define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if `st_blksize' is a member of `struct stat'. */
-#define HAVE_STRUCT_STAT_ST_BLKSIZE 1
+/* #undef HAVE_STRUCT_STAT_ST_BLKSIZE */
 
 /* Define to 1 if your `struct stat' has `st_blksize'. Deprecated, use
    `HAVE_STRUCT_STAT_ST_BLKSIZE' instead. */
-#define HAVE_ST_BLKSIZE 1
+/* #undef HAVE_ST_BLKSIZE */
